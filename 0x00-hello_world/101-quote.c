3caadca63da67ad79bac50b6112be8d3253b5f07
Neba0922 <nebiat.sentayehu@gmail.com>
2023-08-15 02:46:17 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity
@@ -6,6 +5,6 @@
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
